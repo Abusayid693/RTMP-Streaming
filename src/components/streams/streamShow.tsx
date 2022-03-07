f286e8369cfca487896a8d05f12d87c2722f0bda
Rehane <abusayid693@gmail.com>
2022-03-08 03:33:44 +0530

    feat : rtmp server added and flv player

--- src/components/streams/streamShow.tsx
+++ src/components/streams/streamShow.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import FlvJs from 'flv.js';
+import React, { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { fetchStream } from '../../actions/index';
@@ -9,19 +10,39 @@ type IParams = {
 
 const StreamEdit = () => {
   const dispatch = useDispatch();
-  const { id } = useParams<IParams>() as any;
+  const {id} = useParams<IParams>() as any;
+
+  const videoRef = useRef(null);
 
   useEffect(() => {
     dispatch(fetchStream(id));
   }, [id]);
+
+  useEffect(() => {
+    const flvPlayer = FlvJs.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+    // @ts-ignore
+    flvPlayer.attachMediaElement(videoRef.current);
+    flvPlayer.load();
+    console.log(videoRef);
+  }, [videoRef]);
 
   const streams = useSelector((state: any) => state.stream);
   const selecteedStream = streams[id];
 
   return (
     <>
-      <h1>Edit Stream</h1>
-      <h2>{JSON.stringify(selecteedStream)}</h2>
+      <h1>View Stream</h1>
+      <video
+        ref={videoRef}
+        style={{width: '80%', border: '1px solid black'}}
+        controls
+      ></video>
+      <h2>Title : {selecteedStream.title}</h2>
+      <h2>Description : {selecteedStream.description}</h2>
+
     </>
   );
 };
