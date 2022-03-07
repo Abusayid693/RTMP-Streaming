166d314537fe57fe39c622e5e035bf611eabf587
Rehane <abusayid693@gmail.com>
2022-03-08 01:29:37 +0530

    fix : default event propogation

--- src/model.tsx
+++ src/model.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { useNavigate } from 'react-router';
 
 const Model = (props: any) => {
+  const navigation = useNavigate();
+
+  const handleOffSetClick = (e: any) => {
+    navigation('/');
+  };
+
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">knkjnnk</div>
+    <div
+      className="ui dimmer modals visible active"
+      onClick={handleOffSetClick}
+    >
+      <div
+        className="ui standard modal visible active"
+        onClick={(e: any) => e.stopPropagation()}
+      >
+        <div className="header">Delete Stream</div>
+        <div className="content">Are you sure you want to delete?</div>
+        <div className="actions">
+          <button className="ui primary button">Delete</button>
+          <button className="ui button">Cancel</button>
+        </div>
+      </div>
     </div>,
     document.querySelector('#modal') as any,
   );
